934b147989777a8fd9e15bd46969dc5cac73a281
Biswajit Sundara <biswajitsundara@gmail.com>
2023-09-05 21:14:22 +0530

    local storage

--- src/components/Container/MostSimplified/DataApp.jsx
+++ src/components/Container/MostSimplified/DataApp.jsx
@@ -1,16 +1,38 @@
 import { DataSource } from "./DataSource";
-import UserInfo from '../UserInfo';
+import UserInfo from "../UserInfo";
 import axios from "axios";
 
 const DataApp = () => {
-  const getDataFromBackend = async () => {
-    const response = await axios.get("http://127.0.0.1:8080/users/1234");
+  const getDataFromServer = async (url) => {
+    const response = await axios.get(url);
     return response.data;
   };
+
+  const getLocalStorageData = async (key) => {
+    console.log(localStorage.getItem(key));
+    return localStorage.getItem(key);
+  };
+
+  const Text = ({ message }) => <h1>{message}</h1>;
+
   return (
-    <DataSource getDataFunc={getDataFromBackend} resourceName="user">
-      <UserInfo />
-    </DataSource>
+    <>
+      <DataSource
+        getDataFunc={() =>
+          getDataFromServer("http://127.0.0.1:8080/users/1234")
+        }
+        resourceName="user"
+      >
+        <UserInfo />
+      </DataSource>
+
+      <DataSource
+        getDataFunc={()=>getLocalStorageData("message")}
+        resourceName="message"
+      >
+        <Text />
+      </DataSource>
+    </>
   );
 };
 
